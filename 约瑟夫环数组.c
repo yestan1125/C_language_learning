7e99a01a0f42e639065ed88039fbae4bbccb8271
yestan1125 <183602647+yestan1125@users.noreply.github.com>
2024-12-25 17:38:16 +0800

    更新 约瑟夫环数组.c

--- 约瑟夫环数组.c
+++ 约瑟夫环数组.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 
 int main() {
-    int n, m;  // n表示总人数，m表示报数的数字
-    printf("input people:");
-    scanf("%d", &n);
-    printf("input number:");
-    scanf("%d", &m);
+    int n, m, q;
+    scanf("%d %d %d", &n, &m, &q);
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        arr[i] = i + 1;  // 初始化数组，存放人员编号1到n
+    // 特殊情况处理：如果 m=1，所有猴子都会被淘汰
+    if (m == 1) {
+        printf("none\n");
+        return 0;
     }
 
-    int count = 0;  // 记录当前报数
-    int index = 0;  // 当前下标位置
-    int remain = n;  // 剩余人数
+    int monkeys[1000] = {0};
+    for (int i = 1; i <= n; i++) {
+        monkeys[i] = 1;
+    }
 
-    while (remain > 1) {
-        if (arr[index]!= 0) {  // 如果当前位置人员未出圈
-            count++;
-            if (count == m) {  // 报到m
-                arr[index] = 0;  // 将该位置人员标记出圈
-                count = 0;
-                remain--;  // 剩余人数减1
+    int count = n;      // 记录当前剩余的猴子数量
+    int position = q;   // 当前报数位置
+    int number = 1;     // 当前报的数
+
+    while (count > 1) {
+        if (monkeys[position] == 1) {
+            if (number == m) {
+                monkeys[position] = 0;
+                count--;
+                number = 1;
+            } else {
+                number++;
             }
         }
-        index = (index + 1) % n;  // 移动到下一个位置，模拟环形
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (arr[i]!= 0) {  // 找到最后剩下的那个人的编号
-            printf("the last one is %d\n", arr[i]);
-            break;
+        
+        position++;
+        if (position > n) {
+            position = 1;
         }
     }
-
+    
+    if (count == 1) {
+        for (int i = 1; i <= n; i++) {
+            if (monkeys[i] == 1) {
+                printf("%d\n", i);
+                return 0;
+            }
+        }
+    }
+    
+    printf("none\n");
     return 0;
 }
