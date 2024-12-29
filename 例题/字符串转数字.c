1b088d2c32e0646b557e7a07be9c0172a0e6139a
yestan1125 <183602647+yestan1125@users.noreply.github.com>
2024-12-29 12:49:18 +0800

    更新 字符串转数字.c

---
+++ 例题/字符串转数字.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const char *str = "123";
+    int num = atoi(str);
+    printf("转换后的数字: %d\n", num);
+    return 0;
+}
+
+//atoi为标准库中的函数
+
