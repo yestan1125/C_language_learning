1093e3dd4b96dcfbe6ddc8ee7f312c099cbddda5
yestan1125 <183602647+yestan1125@users.noreply.github.com>
2025-03-17 23:10:45 +0800

    Update 查找两个单词链表共同后缀的起始结点.c

--- 数据结构/作业/查找两个单词链表共同后缀的起始结点.c
+++ 数据结构/作业/查找两个单词链表共同后缀的起始结点.c
@@ -37,7 +37,7 @@ void freeList(ListNode *head) {
 // 获取链表长度
 int getLength(ListNode *head) {
     int len = 0;
-    ListNode *curr = head->next;
+    ListNode *curr = head->next;  // 跳过头结点
     while (curr) {
         len++;
         curr = curr->next;
@@ -66,6 +66,39 @@ ListNode* findCommonSuffix(ListNode *str1, ListNode *str2) {
     return NULL;
 }
 
+// 使 str2 共享 str1 的后缀
+void makeSharedSuffix(ListNode *str1, ListNode *str2, char arr2[], int m) {
+    if (m == 0) return;
+    
+    ListNode *p1 = str1->next;
+    ListNode *p2 = str2->next;
+    
+    // 找到 str2 独立部分的最后一个节点
+    for (int i = 0; i < m - 1 && p2; i++) {
+        p2 = p2->next;
+    }
+    
+    // 查找 str1 中与 str2 共享的后缀起点
+    ListNode *suffixStart = str1->next;
+    while (suffixStart) {
+        int match = 1;
+        ListNode *temp1 = suffixStart, *temp2 = p2;
+        while (temp1 && temp2) {
+            if (temp1->val != temp2->val) {
+                match = 0;
+                break;
+            }
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        if (match) {
+            p2->next = suffixStart;  // 共享后缀
+            break;
+        }
+        suffixStart = suffixStart->next;
+    }
+}
+
 int main() {
     int n, m;
     while (scanf("%d %d", &n, &m) == 2 && (n != 0 || m != 0)) {
@@ -76,6 +109,10 @@ int main() {
         ListNode *str1 = createList(arr1, n);
         ListNode *str2 = createList(arr2, m);
         
+        // 共享后缀处理
+        makeSharedSuffix(str1, str2, arr2, m);
+
+        // 查找共同后缀
         ListNode *commonNode = findCommonSuffix(str1, str2);
         if (commonNode) printf("%c\n", commonNode->val);
         
